38b361da3f8ce96c655f21e9951c4cd9e931bd65
1101712 <a1101712@1101712M01.local>
2022-01-10 14:59:56 +0900

    feat: 사용자 입력에 대한 유효성 검사
    사용자 입력 값에 대해 길이, 타입, 중복 검사 후 결과 반환

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,6 @@
 package baseball;
 import nextstep.utils.Randoms;
+import nextstep.utils.Console;
 
 public class Application {
     // 랜덤 넘버 생성
@@ -19,6 +20,44 @@ public class Application {
                 i++;
             }
         }
+    }
+
+    // 사용자 입력 유효성 검사
+    public static boolean isValid(int[] userInput) {
+        // 1-1. 사용자 입력 받아서 배열로 변환
+        // cf. println은 줄바꿈이 생기고, print는 안 생긴다.
+        System.out.print("숫자를 입력해 주세요 : ");
+        char[] input = Console.readLine().toCharArray();
+
+        // 1-2. 중복 값 확인을 위한 used 초기화
+        boolean[] used = new boolean[10];
+
+        // 2-1. 사용자 입력 값의 길이가 3인지 확인
+        if (input.length != 3) {
+            System.out.println("[ERROR] 3자리의 숫자를 입력해 주세요.");
+            return false;
+        }
+
+        // 2-2. 입력 값을 한 자리씩 확이하면서
+        for (int i = 0; i < 3; i++) {
+            // 2-2-1. 입력 값이 모두 숫자인지 확인
+            if (!Character.isDigit(input[i])) {
+                System.out.println("[ERROR] 1부터 9까지 서로 다른 수를 입력해 주세요.");
+                return false;
+            }
+            // 아스키 코드 값으로 정수 변환해 userInput에 할당
+            userInput[i] = input[i] - '0';
+
+            // 2-2-2. 입력 값이 1~9 범위에 있는지 확인
+            // 2-2-3. 중복되는 숫자가 없는지 확인
+            if (userInput[i] < 1 || used[userInput[i]]) {
+                System.out.println("[ERROR] 1부터 9까지 서로 다른 수를 입력해 주세요.");
+                return false;
+            } else {
+                used[userInput[i]] = true;
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args) {
